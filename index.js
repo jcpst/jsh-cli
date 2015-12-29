ba5c4330a6f6e008314b222b801a02012b9aafdf
Joseph Post <jp@jsphpst.com>
2015-12-28 23:42:55 -0600

    add missing 'break' in case and tidy code

--- index.js
+++ index.js
@@ -8,49 +8,50 @@ var args = process.argv.slice(3)
 var cmd = process.argv.slice(2, 3)[0]
 
 var docs = () => {
-  console.log('usage: jsh <cmd> [args]'.red)
-  console.log('Docs: http://shelljs.org/'.red)
+  console.log(colors.red('usage: jsh <cmd> [args]'))
+  console.log(colors.red('Docs: http://shelljs.org/'))
 }
 
 if (process.argv.length < 3) {
   docs()
 } else {
   switch (cmd) {
-    //TODO: Add cases for error, cd, dirs/popd/pushd.
-	case 'cat':
-	case 'grep':
-	case 'pwd':
-	case 'sed':
-	case 'tempdir':
-	case 'test':
+    // TODO: Add cases for [error, cd, dirs, popd, pushd].
+    case 'cat':
+    case 'grep':
+    case 'pwd':
+    case 'sed':
+    case 'tempdir':
+    case 'test':
     case 'which':
       console.log(sh[cmd](...args))
       break
     case 'find':
-	  if (process.argv.length > 3) {
-	    var output = sh[cmd](...args).toString().replace(/,/g, EOL)
-	    console.log(output)
-	  } else {
-	    docs()
-	  }
-	  break
-	case 'ls':
-	  var output = sh[cmd](...args).toString().replace(/,/g, EOL)
-	  console.log(output)
-	  break
-	case 'cd':
+      if (process.argv.length > 3) {
+        var findResult = sh[cmd](...args).toString().replace(/,/g, EOL)
+        console.log(findResult)
+      } else {
+        docs()
+      }
+      break
+    case 'ls':
+      var lsResult = sh[cmd](...args).toString().replace(/,/g, EOL)
+      console.log(lsResult)
+      break
+    case 'cd':
     case 'chmod':
-	case 'cp':
-	case 'echo':
-	case 'exec':
-	case 'ln':
-	case 'mkdir':
-	case 'mv':
-	case 'rm':
+    case 'cp':
+    case 'echo':
+    case 'exec':
+    case 'ln':
+    case 'mkdir':
+    case 'mv':
+    case 'rm':
       sh[cmd](...args)
-	default:
-	  console.log('Incorrect command or command not implemented yet.'.red)
-	  docs()
+      break
+    default:
+      console.log(colors.red('Incorrect command or command not implemented yet.'))
+      docs()
   }
 }
 
