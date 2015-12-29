dbdcb140b22361f2a5e791a1033b15ac93c58c30
Joseph Post <jp@jsphpst.com>
2015-12-28 23:27:22 -0600

    add casses for other commands

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 #!/usr/bin/env node
 
 var sh = require('shelljs')
+var colors = require('colors')
+var EOL = require('os').EOL
 
 var args = process.argv.slice(3)
 var cmd = process.argv.slice(2, 3)[0]
 
+var docs = () => {
+  console.log('usage: jsh <cmd> [args]'.red)
+  console.log('Docs: http://shelljs.org/'.red)
+}
+
 if (process.argv.length < 3) {
-    console.log('usage: jsh <cmd> [args]')
-    console.log('Docs: http://shelljs.org/')
+  docs()
 } else {
   switch (cmd) {
-    case 'pwd':
+    //TODO: Add cases for error, cd, dirs/popd/pushd.
+	case 'cat':
+	case 'grep':
+	case 'pwd':
+	case 'sed':
+	case 'tempdir':
+	case 'test':
     case 'which':
       console.log(sh[cmd](...args))
       break
-    default:
+    case 'find':
+	  if (process.argv.length > 3) {
+	    var output = sh[cmd](...args).toString().replace(/,/g, EOL)
+	    console.log(output)
+	  } else {
+	    docs()
+	  }
+	  break
+	case 'ls':
+	  var output = sh[cmd](...args).toString().replace(/,/g, EOL)
+	  console.log(output)
+	  break
+	case 'cd':
+    case 'chmod':
+	case 'cp':
+	case 'echo':
+	case 'exec':
+	case 'ln':
+	case 'mkdir':
+	case 'mv':
+	case 'rm':
       sh[cmd](...args)
+	default:
+	  console.log('Incorrect command or command not implemented yet.'.red)
+	  docs()
   }
 }
 
